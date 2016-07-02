b1b5187fcf9bfaf3aaf76c6aeb89206187a869bc
jxnguyen <johnxuannguyen@gmail.com>
2016-07-02 15:47:06 +0200

    updated main

--- Q60/TestSuite.java
+++ Q60/TestSuite.java
@@ -1,20 +1,48 @@
 import hanoi.*;
+import java.util.*;
 
 class TestSuite {
 
 	public static void main(String[] args) {
 		try {
-			int[] nums = {1,2,2};
+			System.out.println("Input for this program is expected on two lines:");
+			System.out.println("\t1) The number of discs for the problem.");
+			System.out.println("\t2) A series of space separated numbers in ascending order, signifying the start tower.");
+			// read input
+			Scanner scan = new Scanner(System.in);
+			// to store discs
+			int[] nums = new int[scan.nextInt()];
+			// to store unique discs
+			HashSet<Integer> set = new HashSet<Integer>();
+			// populate array from input
+			for (int i = 0; i < nums.length; i++) {
+				int x = scan.nextInt();
+				nums[i] = x;
+				set.add(x);
+			}
+			// number of unique elems in nums
+			int n = set.size();
+			// init tower & solution objects
 			Tower t = new Tower(nums);
 			HanoiLoesungVerallgemeinert l = new HanoiLoesungVerallgemeinert(t);
-			l.loese(3, 'a', 'b', 'c');
+			// start solution
+			l.loese(n, 'a', 'b', 'c');
+			System.out.printf("Solved in %d moves!\n", t.moves());
 		}
 		catch (TurmException e) {
+			System.out.println(e);
+		}
+		catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 }
 
+// GENERAL SOLUTION
+// ---------------------
+// Solve the Tower of Hanoi problem optimally, where repetitions of
+// discs are permitted/
+//
 class HanoiLoesungVerallgemeinert {
 
 	Tower tower;
@@ -23,12 +51,26 @@ class HanoiLoesungVerallgemeinert {
 		tower = t;
 	}
 
-	void loese (int n, char source, char aux, char target) throws TurmException {
+	// LOESE | Solve the problem optimally, using recursion
+	//
+	void loese (int n, char source, char aux, char target) {
+		// base
 		if (n > 0) {
 			// move n-1 discs from source to aux
-			loese(n -1, source, target, aux);
-			// move 1 disc from source to target
-			tower.move(source, target);
+			loese(n - 1, source, target, aux);
+
+			try {
+				// move 1 disc from source to target
+				int top = tower.move(source, target);
+				// while the next discs are the same size
+				while (!tower.isEmpty(source) && top == tower.top(source)) {
+					// repeat the move
+					tower.move(source, target);
+				}
+			}
+			catch (TurmException e) {
+				System.out.println(e);
+			}
 			// move n-1 disc from aux to target
 			loese(n - 1, aux, source, target);
 		}
