e799ecdf59947e5c133be510daf23d2db6228dee
jxnguyen <johnxuannguyen@gmail.com>
2016-07-02 14:16:16 +0200

    implement recursive solution

--- Q60/TestSuite.java
+++ Q60/TestSuite.java
@@ -3,13 +3,34 @@ import hanoi.*;
 class TestSuite {
 
 	public static void main(String[] args) {
-
+		try {
+			int[] nums = {1,2,2};
+			Tower t = new Tower(nums);
+			HanoiLoesungVerallgemeinert l = new HanoiLoesungVerallgemeinert(t);
+			l.loese(3, 'a', 'b', 'c');
+		}
+		catch (TurmException e) {
+			System.out.println(e);
+		}
 	}
 }
 
 class HanoiLoesungVerallgemeinert {
 
-	static void loese (Tower t) {
-		
+	Tower tower;
+
+	HanoiLoesungVerallgemeinert(Tower t) {
+		tower = t;
+	}
+
+	void loese (int n, char source, char aux, char target) throws TurmException {
+		if (n > 0) {
+			// move n-1 discs from source to aux
+			loese(n -1, source, target, aux);
+			// move 1 disc from source to target
+			tower.move(source, target);
+			// move n-1 disc from aux to target
+			loese(n - 1, aux, source, target);
+		}
 	}
 }
