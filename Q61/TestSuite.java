8681a27525dcb825c4db277e2164f870315241d7
jxnguyen <johnxuannguyen@gmail.com>
2016-07-02 16:35:12 +0200

    implemented test & size

---
+++ Q61/TestSuite.java
@@ -0,0 +1,35 @@
+
+class TestSuite {
+
+	public static void main(String[] args) {
+
+		BinaryTree<Integer> tree = new BinaryTree<Integer>(1);
+		BinaryTree<Integer> a = new BinaryTree<Integer>(2);
+		BinaryTree<Integer> b = new BinaryTree<Integer>(3);
+		BinaryTree<Integer> c = new BinaryTree<Integer>(4);
+		BinaryTree<Integer> d = new BinaryTree<Integer>(5);
+		BinaryTree<Integer> e = new BinaryTree<Integer>(6);
+
+		tree.left = a;
+		tree.right = b;
+		a.left = c;
+		a.right = d;
+		b.left = e;
+
+		System.out.println(size(tree));
+	}
+
+	static int size(BinaryTree t) {
+		return t == null ? 0 : 1 + size(t.left) + size(t.right);
+	}
+}
+
+class BinaryTree <T> {
+
+	T value;
+	BinaryTree<T> left, right;
+
+	BinaryTree(T v) {
+		value = v;
+	}
+}
