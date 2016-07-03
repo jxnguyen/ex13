0426f7e1b6fdfbd3f2e45de1cad980922c1eb8ff
jxnguyen <johnxuannguyen@gmail.com>
2016-07-03 15:02:44 +0200

    import

---
+++ Q62/linkedList/LinkedQueue.java
@@ -0,0 +1,91 @@
+package linkedList;
+
+public class LinkedQueue<T> extends LinkedList<T> {
+
+	Node<T> tail;
+
+	// CONSTRUCTOR
+	//
+	public LinkedQueue() {
+		super();
+	}
+
+	// CONSTRUCTOR | Linked list from array.
+	//
+	public LinkedQueue(T[] array) {
+		super(array);
+	}
+
+	// INSERT | Insert value at beginning of list.
+	//
+	public void insert(T v) {
+		super.insert(v);
+		// if new node is last node
+		if (head.next == null) tail = head;
+	}
+
+	// INSERT | Insert new value after node k & return true, else if k is
+	//				|	not in the list, return false.
+	//
+	public boolean insert(T v, Node<T> k) {
+		// insert node & set tail, if needed
+		boolean success = super.insert(v,k);
+		if (success && k.next.next == null) tail = k.next;
+		return success;
+	}
+
+	// DELETE | Delete head of list & return true, else if list empty
+	//				| return false.
+	//
+	public boolean delete() {
+		// delete head
+		boolean success = super.delete();
+		// update tail if needed
+		if (success && length == 0) tail = null;
+		return success;
+	}
+
+	// DELETE | Delete node k from list & return true, else if k not
+	//			 	| in list, return false.
+	//
+	public boolean delete(Node<T> k) {
+		// delete
+		boolean success = super.delete(k);
+		// if tail deleted
+		if (success && tail == k) {
+			tail = null;
+			// update tail
+			for (Node<T> n : this)
+				if (n.next == null) tail = n;
+		}
+		return success;
+	}
+
+	// APPEND | Append node to end of list
+	//
+	public void append(T v) {
+		// if list empty
+		if (length == 0) insert(v);
+		else {
+			// insert after tail & update tail
+			Node<T> node = new Node<T>(v);
+			tail.next = node;
+			tail = node;
+		}
+	}
+
+	// APPEND | Append linked list to end of list
+	//
+	public void append(LinkedQueue<T> l) {
+		// if list empty, take over list
+		if (length == 0) {
+			head = l.head;
+			tail = l.tail;
+		}
+		else {
+			// concatenate & update tail
+			tail.next = l.head;
+			tail = l.tail;
+		}
+	}
+}
